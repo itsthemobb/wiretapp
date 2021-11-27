782f8594f483d22e6dd70e6787571d7f6b2f3183
Nick Gorman (LCL) <Nick.Gorman@loblaw.ca>
2021-11-27 13:41:38 -0500

    testing the integration for spm into our ui target

--- Package.swift
+++ Package.swift
@@ -1,16 +1,20 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
-
 let package = Package(
     name: "wiretapp",
+    platforms: [
+        .iOS(SupportedPlatform.IOSVersion.v13),
+        .macOS(SupportedPlatform.MacOSVersion.v10_15),
+        .tvOS(SupportedPlatform.TVOSVersion.v13),
+        .watchOS(SupportedPlatform.WatchOSVersion.v6),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "wiretapp",
-            targets: ["wiretapp"]
-        )
+            name: "wiretappTest",
+            targets: ["wiretappTest"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,17 +22,12 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "wiretapp",
-            dependencies: [],
-            path: "wiretapp",
-            exclude: [ "Example/" ]
-        ),
-        .testTarget(
-            name: "wiretappTests",
-            dependencies: ["wiretapp"],
-            exclude: [ "Example/" ]
+            name: "wiretappTest",
+            dependencies: [
+            ],
+            path: "Sources/wiretappTest/"
         ),
     ]
 )
