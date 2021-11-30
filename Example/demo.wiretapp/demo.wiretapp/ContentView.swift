08480846cdadee3bbc4fe1b559506a3b7ea794d1
Nick Gorman <imfractured@gmail.com>
2021-11-30 17:55:16 -0500

    moved some view code into functions and starting to build out more robust demo

--- Example/demo.wiretapp/demo.wiretapp/ContentView.swift
+++ Example/demo.wiretapp/demo.wiretapp/ContentView.swift
@@ -9,36 +9,60 @@ struct Todo: Decodable {
 
 struct ContentView: View {
     @State var cancellable: AnyCancellable!
-    @State var message: String = "Hello, world!"
     @State var todos: [Todo] = []
     let networkService: NetworkService
 
     var body: some View {
         VStack {
+            fetchButton()
             ScrollView {
-                Text(message)
-                    .padding()
-                    .onTapGesture {
-                        self.cancellable = networkService.get(
-                            url: URL(string: "https://jsonplaceholder.typicode.com/todos")!,
-                            modelType: [Todo].self
-                        )
-                        .receive(on: DispatchQueue.main)
-                        .sink {
-                            print($0)
-                        } receiveValue: {
-                            todos = $0
-                        }
-                    }
                 LazyVStack {
                     ForEach(todos, id: \.title)  { todo in
-                        VStack {
-                            Text(todo.title)
-                            Text(String(todo.completed))
-                        }
+                        return todoCell(todo)
                     }
                 }
             }
         }
+    }
+}
+
+extension ContentView {
+    fileprivate func todoCell(_ todo: Todo) -> some View {
+        VStack {
+            HStack {
+                Image(systemName:
+                        todo.completed
+                      ? "checkmark.circle.fill"
+                      : "x.circle.fill"
+                )
+                Text(todo.title)
+                    .frame(
+                        maxWidth: .infinity,
+                        alignment: .leading
+                    )
+            }
+            Divider()
+        }
+        .padding(4)
+    }
+    
+    fileprivate func fetchButton() -> some View {
+        Text("Fetch To-Do's")
+            .foregroundColor(Color.white)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(8)
+            .onTapGesture {
+                self.cancellable = networkService.get(
+                    url: URL(string: "https://jsonplaceholder.typicode.com/todos")!,
+                    modelType: [Todo].self
+                )
+                .receive(on: DispatchQueue.main)
+                .sink {
+                    print($0)
+                } receiveValue: {
+                    todos = $0
+                }
+            }
     }
 }
